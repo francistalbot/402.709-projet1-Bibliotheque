55ffdec4eb9ca3f50ce860627e570a705e92cd7b
francistalbot <francistalbot@hotmail.com>
2025-09-05 16:50:22 -0400

    styling of livres.show

--- resources/views/livres/show.blade.php
+++ resources/views/livres/show.blade.php
@@ -3,36 +3,65 @@
 @section('title', 'Details du Livre')
 
 @section('content')
-    <h2>Détails du Livre</h2>
-    <a href="{{ route('livres.edit',['livre' => $livre->id]) }}">Éditer le Livre</a>
-    <form action="{{ route('livres.destroy',['livre' => $livre->id]) }}"
-            method="POST" style="display:inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce livre ?')">Supprimer le Livre</button>  
-    </form>
+    <div class="py-1 py-md-3">
+        <h2>Détails du Livre</h2>
+    </div>
     <hr>
-    <div>
-        <strong>Titre:</strong> {{ $livre->titre }}
+    <div class="mb-3 row ">
+        <div class="col  mb-3 mb-md-0">
+            <a class="col btn btn-primary" href="{{ route('livres.edit',['livre' => $livre->id]) }}">Éditer le Livre</a>
+        </div>
+        <form class="col " action="{{ route('livres.destroy',['livre' => $livre->id]) }}"
+                method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce livre ?')">Supprimer le Livre</button>
+        </form>
     </div>
-    <div>
-        <strong>Auteur:</strong> {{ $livre->auteur->name }}
+    <div class="m-3 px-5 pb-5 border bg-white w-50 mx-auto text-start">
+        <div class="mx-3  text-start">
+            <div class="m-3">
+                <div class="w-fit bg-dark bg-opacity-75 rounded text-white p-2">
+                 Science-fiction</div>
+            </div>
+            <div class="mb-3">
+                <h1 class="mb-0 mx-3">
+                    <strong>{{ $livre->titre }}</strong> 
+                </h1>
+                <div class="mb-2 text-muted mx-3">
+                    <small>ISBN {{ $livre->isbn }}</small>
+                </div>
+            </div>
+            <div class="m-3 ">
+
+                 <h4 class="fst-italic fw-bold">{{ $livre->prix }}$</h4>
+            </div>   
+            <div class="row">
+                <div class=" col">
+                    <strong>Auteur:</strong> {{ $livre->auteur->name }}
+                </div>
+                <div class=" col">
+                    <strong>Année:</strong> {{ $livre->publication_annee }}
+                </div>    
+                <div class="mb-2">
+                    <strong>Résumé:</strong> 
+                    <blockquote class="blockquote mx-5 text-break fst-italic fw-light display-1" style="
+                        -webkit-line-clamp: 5;
+                        display: -webkit-box;
+                        -webkit-box-orient: vertical;
+                        overflow: hidden;">
+                        <small>Dans un futur lointain, l'humanité a colonisé de nombreuses planètes à travers la galaxie. Sur la planète Zorax, une jeune scientifique nommée Lyra découvre une ancienne technologie extraterrestre qui pourrait changer le destin de l'univers. Cependant, elle doit faire face à des forces obscures qui cherchent à s'emparer de cette technologie pour des fins sinistres. Avec l'aide d'un groupe hétéroclite d'alliés, Lyra entreprend un voyage périlleux pour protéger la découverte et révéler la vérité sur les origines de l'humanité.
+                        </small>
+                    </blockquote>
+                </div>
+            </div>
+            
+        </div>
+         <a class="btn btn-outline-primary" href="{{ route('livres.edit',['livre' => $livre->id]) }}">Ajouter au panier</a>
+                    
     </div>
-    <div>
-        <strong>Catégorie:</strong> {{ $livre->categorie->name }}
+    <div class="text-start">
+        <a  href="{{ route('livres.index') }}">Retour à la liste des livres</a>
     </div>
-    <div>
-        <strong>Année de Publication:</strong> {{ $livre->publication_annee }}
-    </div>
-    <div>
-        <strong>ISBN:</strong> {{ $livre->isbn }}
-    </div>
-    <div>
-        <strong>Résumé:</strong> {{ $livre->resume }}
-    </div>
-    <div>
-        <strong>Prix:</strong> {{ $livre->prix }}$
-    </div>
-    <a href="{{ route('livres.index') }}">Retour à la liste des livres</a>
 
 @endsection
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light border-bottom bg-white py-3 ">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!"> Bibliothèque à Livres</a>
+        <a class="navbar-brand" href="{{ route('home') }}"> Bibliothèque à Livres</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a></li>
