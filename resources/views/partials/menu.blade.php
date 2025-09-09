8157d6a2b705bfad7fbec2209ea9f30c37bbdcd2
francistalbot <francistalbot@hotmail.com>
2025-09-08 21:41:26 -0400

    styling of messages page

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -4,7 +4,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('home') }}">Accueil</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact nous</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contactez-nous</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('nouveautes') }}">Nouveautés</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('messages.index') }}">Messages</a></li>
             </ul>
