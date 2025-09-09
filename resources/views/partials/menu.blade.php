35eae49605f5c808ed87d29d664714a0037480e2
francistalbot <francistalbot@hotmail.com>
2025-09-08 22:07:33 -0400

    add menu-burger

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light border-bottom bg-white py-3 ">
     <div class="container px-4 px-lg-5">
         <a class="navbar-brand" href="{{ route('home') }}"> Bibliothèque à Livres</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('home') }}">Accueil</a></li>
