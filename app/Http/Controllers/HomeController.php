1e32ae55a7c2317e5c45f6817a4e2dea592a72e8
francistalbot <francistalbot@hotmail.com>
2025-09-24 20:02:59 -0400

    removes messages in the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 use App\Models\Livre;
-use App\Models\Message;
-
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,25 +21,5 @@ class HomeController extends Controller
     {
         $livres = Livre::where('created_at', '>=', now()->subDays(10))->get();
         return view('pages.nouveautes', compact('livres'));
-    }
-    public function showMessages()
-    {
-        $messages = Message::all();
-        return view('pages.messages', compact('messages'));
-    }
-    public function storeMessage(Request $request)
-    {
-        // Validation des données
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'texte' => 'required|string',
-        ]);
-
-        // Création du message
-        Message::create($validatedData);
-        echo "Message créé avec succès !";
-
-        return redirect()->route('contact')->with('success', 'Votre message a été envoyé avec succès !');
     }
 }
